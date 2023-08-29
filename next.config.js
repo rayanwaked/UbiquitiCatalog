dac5f6afd18f3f2bc562190e59d848f048dfee68
Rayan Waked <rayanwaked@icloud.com>
2023-08-28 17:09:57 -0700

    Custom UI Sans font added manifest.json for mobile added Icons added

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+const withPWA = require('next-pwa');
 
 module.exports = nextConfig
 
-module.exports = {
+// module.exports = {
+//     images: {
+//         domains: ['static.ui.com'],
+//     },
+// };
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+    },
     images: {
         domains: ['static.ui.com'],
     },
-};
+});
