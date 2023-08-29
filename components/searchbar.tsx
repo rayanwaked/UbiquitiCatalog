ea81c04a5d6b202e2f777b4630277aacf8a62a4b
Rayan Waked <rayanwaked@icloud.com>
2023-08-29 14:47:41 -0700

    Creating search bar, modified list

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -6,6 +6,7 @@ export default function SearchBar() {
     return (
         <div className={"searchBarContainer"}>
             <div className={"searchBar"}>
+                <div className={"searchBarIcon"}></div>
                 <p>Search</p>
             </div>
             <p className={"searchBarCount"}>123 Devices</p>
